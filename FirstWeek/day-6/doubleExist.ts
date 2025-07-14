c1a8039b223d999273351c3540e1b58e77d5e8fa
willianwt2000 <willianmetelien@gmail.com>
2025-07-14 00:01:39 -0400

    feature/ day-7 find difference

--- FirstWeek/day-6/doubleExist.ts
+++ FirstWeek/day-6/doubleExist.ts
@@ -1,27 +1,18 @@
 function checkIfExist(arr: number[]): boolean {
-    for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
+    const numberOne = arr[i];
 
-      const numberOne = arr[i];
-      
-      for (let j = 0; j < arr.length; j++) {
-        const numberTwo = arr[j];
-        
+    for (let j = 0; j < arr.length; j++) {
+      const numberTwo = arr[j];
 
-
-        if (i !== j && numberOne === 2 * numberTwo) {
-          return true;
-        }
-
+      if (i !== j && numberOne === 2 * numberTwo) {
+        return true;
       }
-      
     }
+  }
 
   return false;
-};
+}
 
-
-
-
-
-let input: number[] = [10,2,5,3];
+let input: number[] = [10, 2, 5, 3];
 console.log(checkIfExist(input)); //true
